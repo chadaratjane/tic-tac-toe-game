e155919dd1ed51d6fe30c6d0f7c01d4fae94184f
chadaratjane <chadarat.punyanukul@gmail.com>
2021-07-06 13:28:09 +0700

    Add Error handler, error response

---
+++ src/main/java/com/demo/tictactoe/exception/ErrorHandler.java
@@ -0,0 +1,56 @@
+package com.demo.tictactoe.exception;
+
+import com.demo.tictactoe.model.response.CommonResponse;
+import com.demo.tictactoe.model.response.ErrorResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    private static final Logger logger = LogManager.getLogger(ErrorHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponse> handlerAllError (Exception e){
+        logger.error("UNEXPECTED ERROR",e);
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setStatus("INTERNAL SERVER ERROR");
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError("INTERNAL SERVER ERROR");
+        commonResponse.setData(errorResponse);
+        commonResponse.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(commonResponse,commonResponse.getHttpStatus()) ;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CommonResponse> handlerValidation(MethodArgumentNotValidException e) {
+        logger.error("BAD REQUEST",e);
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setStatus("BAD REQUEST");
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError(e.getFieldError().getDefaultMessage());
+        commonResponse.setData(errorResponse);
+        commonResponse.setHttpStatus(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(commonResponse,commonResponse.getHttpStatus()) ;
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<CommonResponse> handlerDataAccess(DataAccessException e){
+        logger.error("DATA ACCESS FAILED",e);
+        CommonResponse commonResponse = new CommonResponse();
+        commonResponse.setStatus("DATA ACCESS FAILED");
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setError("DATA ACCESS FAILED");
+        commonResponse.setData(errorResponse);
+        commonResponse.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(commonResponse,commonResponse.getHttpStatus());
+    }
+
+}
--- src/main/java/com/demo/tictactoe/model/request/PlayerLoginRequest.java
+++ src/main/java/com/demo/tictactoe/model/request/PlayerLoginRequest.java
@@ -1,8 +1,16 @@
 package com.demo.tictactoe.model.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class PlayerLoginRequest {
 
+    @NotBlank(message = "player name is invalid")
+    @Pattern(regexp="^[A-Za-z- ]*$",message = "player name is invalid,please input alphabet")
     private String playerName;
+
+    @NotNull(message = "player age is invalid")
     private Integer playerAge;
 
     public String getPlayerName() {
